7a128773b722c360f42ee3c6665593ec4494dd71
yu-he <yuhe418088765@outlook.com>
2023-09-06 15:01:39 +0800

    新增集成logback

--- spid-web/src/main/java/com/yuhe/spid/web/fil/TestController.java
+++ spid-web/src/main/java/com/yuhe/spid/web/fil/TestController.java
@@ -1,11 +1,16 @@
 package com.yuhe.spid.web.fil;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 public class TestController {
     @GetMapping("/index")
     public String index(){
+        log.debug("debugTest");
+        log.info("infoTest");
+        log.error("errorTest");
         return "index";
     }
 }
